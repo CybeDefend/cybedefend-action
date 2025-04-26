6051091da38cba7a68be8b9d1df822ca5cb0bf1d
Florentin Ledy <26773259+FlorentinLedy@users.noreply.github.com>
2025-04-26 17:17:12 +0200

    fix: error in inputs

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,4 @@ set -e
 export CYBEDEFEND_API_KEY="${INPUT_API_KEY}"
 export CYBEDEFEND_PROJECT_ID="${INPUT_PROJECT_ID}"
 
-echo "Running: /app/cybedefend scan --ci --dir ."
 /app/cybedefend scan --ci --dir .
